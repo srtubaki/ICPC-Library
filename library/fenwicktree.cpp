428b5bc7869151bbf71af253ecf3755d6f503d7a
haruki <haruki4936@icloud.com>
2022-12-13 17:37:48 +0900

    fenwick tree (Binary Indexed Tree) を追加

---
+++ library/fenwicktree.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+template <class T> struct fenwick_tree {
+
+  public:
+    fenwick_tree() : _n(0) {}
+    fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += T(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+  private:
+    int _n;
+    std::vector<T> data;
+
+    T sum(int r) {
+        T s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
